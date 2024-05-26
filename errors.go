7f166168a9f6d67d5353400105ac3a574dce74ff
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-05-26 15:54:23 +0700

    package errors

---
+++ errors.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ValidationError = errors.New("validation error")
+	NotFoundError   = errors.New("not found error")
+)
+
+func GetById(id string) error {
+	if id == "" {
+		return ValidationError
+	}
+
+	if id != "Bayu" {
+		return NotFoundError
+	}
+
+	// sukses
+
+	return nil
+}
+
+func main() {
+	err := GetById("Bayu")
+	if err != nil {
+		if errors.Is(err, ValidationError) {
+			fmt.Println("validation error")
+		} else if errors.Is(err, NotFoundError) {
+			fmt.Println("not found error")
+		} else {
+			fmt.Println("unknown error")
+		}
+		} else {
+		// sukses
+		fmt.Println("Sukses")
+
+	}
+}
